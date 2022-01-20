705fd689acbc3b0e89f6c569062e5bd50d87c48d
ospace <ospace114@empal.com>
2022-01-21 01:28:51 +0900

    well [mybatis] enable camel case

--- src/main/java/com/tistory/ospace/test/TestApplication.java
+++ src/main/java/com/tistory/ospace/test/TestApplication.java
@@ -2,17 +2,13 @@ package com.tistory.ospace.test;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.tistory.ospace.test.annotation.EnableTimeLog;
 
 @EnableTimeLog
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableTransactionManagement
 public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication springApplication = new SpringApplication(TestApplication.class);
--- src/main/java/com/tistory/ospace/test/configuration/SpringMvcConfig.java
+++ src/main/java/com/tistory/ospace/test/configuration/SpringMvcConfig.java
@@ -1,18 +1,24 @@
 package com.tistory.ospace.test.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
 public class SpringMvcConfig implements WebMvcConfigurer {
 	
-//	@Override
-//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//		registry.addResourceHandler("/static/**")
-//			.addResourceLocations("classpath:/static/").setCachePeriod(31536000);
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/").setCachePeriod(31536000);
 //		registry.addResourceHandler("/favicon.ico")
 //			.addResourceLocations("classpath:/static/favicon.ico").setCachePeriod(31536000);
-//	}
+	}
 
+	@Bean
+    public QueryInterceptor queryInterceptor() {
+        return new QueryInterceptor();
+    }
 }
